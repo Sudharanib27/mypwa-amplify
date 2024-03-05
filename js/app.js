2a24b2b9ca287d831fdbb8f771099012443d5f87
Sudharani <sudharanib27@gmail.com>
2024-03-04 19:05:34 -0600

    added call api

--- js/app.js
+++ js/app.js
@@ -19,14 +19,30 @@ document
     const id = document.getElementById("id").value;
 
     try {
-      // Call the API to create a new item
-      const response = await API.post("apiName", "/items", {
-        body: {
-          name: name,
-          id: id,
-        },
-      });
-
+      // callAPI function that takes the base and exponent numbers as parameters
+      var callAPI = (id, name) => {
+        // instantiate a headers object
+        var myHeaders = new Headers();
+        // add content type header to object
+        myHeaders.append("Content-Type", "application/json");
+        // using built in JSON utility package turn object to string and store in a variable
+        var raw = JSON.stringify({ id: id, name: name });
+        // create a JSON object with parameters for API call and store in a variable
+        var requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: raw,
+          redirect: "follow",
+        };
+        // make API call with parameters and use promises to get response
+        fetch(
+          "https://itvwkp7335.execute-api.us-east-2.amazonaws.com/beta/mypwa2024",
+          requestOptions
+        )
+          .then((response) => response.text())
+          .then((result) => alert(JSON.parse(result).body))
+          .catch((error) => console.log("error", error));
+      };
       // Log the response
       console.log("API Response:", response);
 
